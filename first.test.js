2c8fa974dcc87b41ee90837e7a453b5e76241e50
Nicolas Corbalan <outreef@hotmail.com>
2021-07-28 20:09:19 +0200

    using matches

--- first.test.js
+++ first.test.js
@@ -1,14 +1,11 @@
 let myValue = 1;
-let numbers = [
-  [1, 2, 3],
-  [2, 2, 4],
-  [4, 5, 9]
-]
+let name = "Christian";
 
-function add(x, y) {
-  return x + y;
-}
 
-test.each(numbers)("Add %i to %i should be %i", (a, b, total) => {
-  expect(add(a,b)).toBe(total);
+test("First", () => {
+  expect(myValue).toBe(1);
+  expect(myValue).toEqual(1);
+
+  expect(myValue).toBeGreaterThan(0);
+  expect(name).toMatch(/Chris/);
 })
