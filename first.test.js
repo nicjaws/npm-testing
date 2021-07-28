f864dc0f2689e7ae91ca9a986de45b5963d1f784
Nicolas Corbalan <outreef@hotmail.com>
2021-07-28 13:09:09 +0200

    using loops in arrays

--- first.test.js
+++ first.test.js
@@ -1,9 +1,14 @@
 let myValue = 1;
+let numbers = [
+  [1, 2, 3],
+  [2, 2, 4],
+  [4, 5, 9]
+]
+
 function add(x, y) {
   return x + y;
 }
-test("First", () => {
-  let myValue2 = 3;
-  expect(add(2,3)).toBe(5);
-  expect(add(4,5)).toBe(9);
+
+test.each(numbers)("Add %i to %i should be %i", (a, b, total) => {
+  expect(add(a,b)).toBe(total);
 })
